e4e73643fe14c49d0c928c75da159f3ea71a7cff
Alejandro Pérez <aleperez92@gmail.com>
2013-10-08 19:59:07 +0100

    Added: methods

--- src/moneycalculator/Number.java
+++ src/moneycalculator/Number.java
@@ -11,12 +11,40 @@ public class Number {
         this.denominator = denominator;
     }
 
+    public Number(Number number) {
+        this(number.numerator, number.denominator);
+    }
+    
+    public Number(long number) {
+        this(number, 1);
+    }
+
+    public Number(double number) {
+        
+    }
+    
     public long getNumerator() {
         return numerator;
     }
 
     public long getDenominator() {
         return denominator;
+    }
+    
+    private void reduce(){
+        
+    }
+    
+    @Override
+    public boolean equals(Object object) {
+        if(object == null) return false;
+        if(object instanceof Number) return equals((Number) object);
+        
+        return false;
+    }
+    
+    private boolean equals(Number number) {
+        return (numerator == number.numerator) && (denominator == number.denominator);
     }
     
 }
